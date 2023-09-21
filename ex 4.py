b3ff86a7d937b1fe4defbd7c8d1a3b82519f8516
henriquepereira24 <henriquesilvapereira@enta.pt>
2023-09-21 15:38:57 +0000

    update

--- ex 4.py
+++ ex 4.py
@@ -4,13 +4,26 @@ Quantidade numeros numa lista entre 0 e 10, 11 - 20, 21 - 30, 31 - 40, 41 - 50 d
 
 list = [10, 8, 80, 49, 20, 41, 20, 25, 34, 27]
 
-a=0
-b=0
-c=0
-d=0
-e=0
-f=0
+range_0_10 = 0
+range_11_20 = 0
+range_21_30 = 0
+range_31_40 = 0
+range_41_50 = 0
 
-for num in list:
-    if num <= 10:
-        a = a + 1
+for number in list:
+    if 0 <= number <= 10:
+        range_0_10 += 1
+    elif 11 <= number <= 20:
+        range_11_20 += 1
+    elif 21 <= number <= 30:
+        range_21_30 += 1
+    elif 31 <= number <= 40:
+        range_31_40 += 1
+    elif 41 <= number <= 50:
+        range_41_50 += 1
+
+print("Number of numbers between 0 and 10:", range_0_10)
+print("Number of numbers between 11 and 20:", range_11_20)
+print("Number of numbers between 21 and 30:", range_21_30)
+print("Number of numbers between 31 and 40:", range_31_40)
+print("Number of numbers between 41 and 50:", range_41_50)
