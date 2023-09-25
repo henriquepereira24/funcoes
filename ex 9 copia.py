5f28c745a94e972cd8835889f43c9c9af0a1c422
henriquepereira24 <henriquesilvapereira@enta.pt>
2023-09-25 21:19:23 +0000

    update de alguns exercicios em casa

--- ex 9 copia.py
+++ ex 9 copia.py
@@ -1,8 +1,12 @@
+# lista os nomes das ilhas
 ilhas = ["Pico", "Faial", "São Jorge", "Terceira", "Graciosa"]
+# lista com 5 casas para vendas de cada ilha
 vendas = [0, 0, 0, 0, 0]
 
+# varias variaveis para armazenar o total , minimo e maximo
 total = minimo = maximo = casa = 0
 
+# loop solicita ao utilizador que insira o valor de vendas para cada ilha, também calcula o total das vendas e atualiza o minimo e o maximo conforme necessário
 for i in range(len(ilhas)):
     vendas[casa] = (int(input(f"Digite o valor de {ilhas[i]}: ")))
     total += vendas[casa]
@@ -15,7 +19,7 @@ for i in range(len(ilhas)):
         if vendas[casa] > maximo:
             maximo = vendas[casa]
     casa += 1
-
+# imprime uma lista das vendas, total de vendas, o valor minimo e o valor maximo.
 print(f"vendas: {vendas} total: {total} minimo: {minimo} maximo: {maximo}")
 
 print(f"ilhas com mais vendas [{maximo}]")
@@ -28,13 +32,11 @@ for casa in range(len(vendas)):
     if vendas[casa] == minimo:
         print(ilhas[casa])
 
+media =
+for casa in range(len(vendas)):
+    if
 
 
-if vendas[x] > vendas[x+1]:
-    suporte = vendas[x]
-    vendas[x] = vendas[x+1]
-    vendas[x+1] = suporte
-    troquei = True
 
 
 
--- ex lista4.py
+++ ex lista4.py
@@ -1,23 +1,25 @@
+# lista do alunos
 alunos = ["Maria", "João", "Pedro","Isabel"]
+# lista de notas
 notas = [8.5, 7.5, 6.5, 5]
-
+# média dos alunos
 media = sum(notas) / len(notas)
-
+# print media
 print(f"A média das notas é de {media}")
-
+# duas variáveis, aluno_maior_nota e notas_maior_nota, com os valores do primeiro aluno (Maria) e sua nota correspondente (8.5).
 aluno_maior_nota= alunos[0]
 notas_maior_nota = notas[0]
-
+# for para procurar os alunos com maior nota
 for i in range(len(alunos)):
     if notas[i] > notas_maior_nota:
         aluno_maior_nota = alunos[i]
         notas_maior_nota = notas[i]
 
 print(f"O melhor aluno foi {aluno_maior_nota} e a nota foi de {notas_maior_nota}")
-
+#duas variáveis, aluno_menor_nota e notas_menor_
 aluno_menor_nota = alunos[0]
 notas_menor_nota = notas[0]
-
+# for para procurar os alunos com menor nota
 for i in range(len(alunos)):
     if notas[i] < notas_menor_nota:
         aluno_menor_nota = alunos[i]
@@ -28,5 +30,7 @@ print(f"O pior aluno foi {aluno_menor_nota} e a nota foi de {notas_menor_nota}")
 for i in range(len(alunos)):
     if notas[i] > media:
         print(f"O aluno {alunos[i]} foi aprovado com {notas[i]}")
+    if notas[i] < media:
+        print(f"O aluno {alunos[i]} foi reprovado com {notas[i]}")
 
 
