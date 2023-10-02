731573ef0e5296ec7d13f4c6f749682d96704c6d
ciberseguranca <henriquesilvapereira@enta.pt>
2023-10-02 08:42:24 +0000

    Exercicios da aula

--- ex 16 - calcular salario.py
+++ ex 16 - calcular salario.py
@@ -6,77 +6,44 @@
 #       61 - ... Preço 2.0 x
 # Vendas: 10000 - 1%, 20000 - 2%, 30000 - 3%, 40000 - 4%, ... %5
 
-nome = input('Como se chama:')
-horas = int(input('Quantas horas trabalhou:'))
-preco_hora= float(input('Quanto ganhas à hora:'))
-vendas = int(input('Quanto vendeu:'))
+seguranca_social = 0.11
+vendaextra = 0
+salario = 0
 
+nome = input('Como te chamas?')
+horas = float(input('Quantas horas trabalhas?'))
+ganho = float(input('Quanto ganhas por hora?'))
+venda = float(input('Quanto que tu vendeste?'))
 
-#nome = input('Como te chamas?'))
-#horas = int(input('quantas horas trabalhadas?'))
-#vendas = float(input('quantas vendas?'))
-#por_hora = float(input('preco por hora?'))
+if 0 < venda <= 10000:
+    vendaextra = venda * 0.01
+elif 10001 < venda <= 20000:
+    vendaextra = venda - 10000
+    vendaextra *= 0.02
+    vendaextra += 100
+elif 20000 < venda <= 30000:
+    vendaextra = venda - 20000
+    vendaextra *= 0.03
+    vendaextra += 300
+elif 30000 < venda <= 40000:
+    vendaextra = venda - 30000
+    vendaextra *= 0.04
+    vendaextra += 600
+elif venda >= 40000:
+    vendaextra = venda - 40000
+    vendaextra *= 0.05
+    vendaextra += 1000
 
+if horas <= 40:
+    salario = horas * ganho
+if 41 <= horas <= 60:
+    salario = 40 * ganho + (horas - 40) * ganho * 1.5
+if horas >= 61:
+    salario = 40 * ganho + 20 * ganho * 1.5 + (horas - 60) * ganho * 2
 
+salario += vendaextra
+desconto = salario * seguranca_social
+salario -= desconto
 
-
-
-segurancaSocial = 0.11
-percentagem_de_vendas = 0.0
-desconto = 0.0
-salarioLiquido = 0
-salarioBruto = 0.0
-
-
-
-#desconto
-
-resto = 0
-resto_de_vendas = vendas
-
-i = 0.01
-if vendas >= 10000:
-    resto = 10000 * i
-    i += 0.01
-    resto_de_vendas = resto_de_vendas - 10000
-if vendas >= 20000:
-    resto += (10000 * i)
-    i += 0.01
-    resto_de_vendas = resto_de_vendas - 10000
-if vendas >= 30000:
-    resto += (10000 * i)
-    i += 0.01
-    resto_de_vendas = resto_de_vendas - 10000
-if vendas >= 40000:
-    resto += (10000 * i)
-    i += 0.01
-    resto_de_vendas = resto_de_vendas - 10000
-if resto != 0:
-    resto += (resto_de_vendas * i)
-    i += 0.01
-print(resto_de_vendas)
-print(resto)
-
-
-#preco por hora
-if horas  <= 40:
-    print('horas trabalhadas: 0 - 40 preco normal')
-    salarioBruto = horas * preco_hora
-    salarioBruto = salarioBruto + (vendas * percentagem_de_vendas)
-    desconto = salarioBruto * segurancaSocial
-    salarioLiquido = salarioBruto - desconto
-elif horas <= 60:
-    print('horas trabalhadas: 41 - 60 1.5x')
-    salarioBruto = 40 * preco_hora
-    resto_das_horas = horas - 40
-    salarioBruto = salarioBruto + resto_das_horas * preco_hora * 1.5
-    desconto = salarioBruto * segurancaSocial
-
-else:
-    print('horas trabalhadas: 61 < preco 2.0x')
-
-salarioBruto = salarioBruto + resto
-
-print(f'salarioBruto-{salarioBruto}')
-print(f'desconto-{desconto}')
-print(f'salarioLiquido-{salarioLiquido}')
+print(
+    f'Nome: {nome}\nTrabalhas: {horas} horas\nGanhas: {ganho}€ por hora\nVendeste: {venda}\n\nRecebeste o bonus de vendas de: {vendaextra}€\nSalario final: {salario}€')
